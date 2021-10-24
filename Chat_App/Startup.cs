2d16691fdcff8a036ed6b73fbfd8ae5992ca9a76
Yakov Kantor <80357569+yakov10105@users.noreply.github.com>
2021-10-24 14:06:23 +0300

    1

--- Chat_App/Startup.cs
+++ Chat_App/Startup.cs
@@ -27,6 +27,8 @@ namespace Chat_App
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
