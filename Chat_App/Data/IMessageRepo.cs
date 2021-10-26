030db6c594cfa4ca40c2964709af89cfae27fafb
mywork.fun111 <mywork.fun111@gmail.com>
2021-10-26 15:16:03 +0300

    First Repository

---
+++ Chat_App/Data/IMessageRepo.cs
@@ -0,0 +1,14 @@
+﻿using Chat_App.Models;
+using System.Collections.Generic;
+using System;
+
+
+namespace Chat_App.Data
+{
+    public interface IMessageRepo
+    {
+        IEnumerable<Message> GetAllMessages();
+    }
+}
+
+
---
+++ Chat_App/Data/IUserRepo.cs
@@ -0,0 +1,13 @@
+using Chat_App.Models;
+using System.Collections.Generic;
+using System;
+
+
+namespace Chat_App.Data
+{
+	public interface IUserRepo
+	{
+		IEnumerable<User> GetAllUsers();
+	}
+}
+
