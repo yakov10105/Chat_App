4b3b71e9d804557627fea9a61e239ebaad540f81
Yakov Kantor <80357569+yakov10105@users.noreply.github.com>
2021-10-26 11:19:09 +0300

    Models

---
+++ Chat_App/Models/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_App.Models
+{
+    public class Message
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MessageId { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Message cannot be empty.")]
+        [StringLength(300, ErrorMessage = "Max Message length : 300 chars.")]
+        [DataType(DataType.MultilineText)]
+        public string Text { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+
+        [ForeignKey("Sender"),Column(Order =0)]
+        public int SenderId { get; set; }
+
+        [ForeignKey("Reciever"), Column(Order = 1)]
+        public int RecieverId { get; set; }
+
+        public virtual User Sender { get; set; }
+
+        public virtual User Reciever { get; set; }
+    }
+}
---
+++ Chat_App/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Chat_App.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="First Name field is required.")]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name field is required.")]
+        [StringLength(30)]
+        public string LastName { get; set; }
+
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "User Name field is required.")]
+        [StringLength(15)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Age field is required.")]
+        [Range(0, 120, ErrorMessage = "Age must be between 0-120.")]
+        [Display(Name = "Age")]
+        [DataType("Int32", ErrorMessage = "The input must be a natural number.")]
+        public int UserAge { get; set; }
+
+        [Required(ErrorMessage ="Email field is required.")]
+        [Display(Name ="Email")]
+        [EmailAddress(ErrorMessage = "The input must be valid Email adrress")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Password field is required.")]
+        [Display(Name ="Password")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password field is required.")]
+        [Display(Name ="Confirm Password")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Incompatible passwords.")]
+        public string ConfirmPassword { get; set; }
+
+        public bool IsOnline { get; set; }
+
+        public int WinCoins { get; set; } = 0;
+
+        public IEnumerable<Message> Messages { get; set; }
+
+        public IEnumerable<User> Friends { get; set; }
+
+        public IEnumerable<Room> Rooms { get; set; }
+    }
+}
